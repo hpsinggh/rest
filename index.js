058db11a1afc8ecd70079428657dc75f4bc6fd54
manan2809 <maninder2809@hotmail.com>
2018-05-10 00:05:50 +0530

    adding files

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('public')); //Public folder name
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('server is ready!');
 })
